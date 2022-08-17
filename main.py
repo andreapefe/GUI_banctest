5ac46b5c68a120686b043c4c76e25588c58fe590
Andrea Perez <stage22@batconnect.fr>
2022-08-17 09:41:46 +0200

    v0.1 with bugs

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def disconnect():
         "Closed without Using it -_-"
 
     #application is closed -> update to only close connection and not kill the interface
-    root.close()
+    root.quit()
 
 def send():
     send_data = data_entry.get()
@@ -166,38 +166,3 @@ if __name__ == "__main__":
     #main loop
     root.geometry('1000x780+100+50')
     root.mainloop()
-
-    #create object to view objects
-    # root.columnconfigure(0, weight=1)
-    # root.columnconfigure(1, weight=1)
-    #
-    # #base font and variables
-    # font = ("Futura", 12)
-    # serial_port = tk.StringVar()
-    #
-    # #attributes of the window
-
-    # #ajout du logo batconnect à la fenêtre
-    # #root.iconbitmap("logo-batconnect-vertical_1_.ico")
-    #
-    # # place labels and buttons on the root window
-    # ttk.Label(root, text="Application de Banc de test pour tracker et batterie", font=("Futura", 14)).grid(row=0,column=0, columnspan=2, ipady=10, sticky=tk.NS)
-    #
-    # ttk.Label(root, text="Choisir Mode", font=font).grid(row=1, column=0)
-    # mode = tk.StringVar(value=("Mode Supervision", "Mode Simulation"))
-    # mode = tk.Listbox(root, listvariable=mode, height=2, selectmode='browse').grid(row=1, column=1, pady=30)
-    #
-    # ttk.Label(root, text="Port Serial", font=font).grid(row=2, column=0, pady=30)
-    # textbox = ttk.Entry(root, textvariable=serial_port).grid(row=2, column=1)
-    # ttk.Button(root, text="Se Connecter", command=lambda : f.connect(serial_port.get())).grid(row=3, column=0, columnspan=2, pady=30)
-    #
-    #
-    # ttk.Label(root, text="Andrea Pérez Fernández, Léa Scheer - 2022", font=("Futura", 8)).grid(column=0, row=4, pady=40, sticky=tk.SW)
-    #
-    #
-    # #f.read_serial()
-    # #Maintain window open
-    #
-    # while True:
-    #     root.update()
-
